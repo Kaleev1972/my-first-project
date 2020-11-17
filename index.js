cd0e65f3f42e14241effd176d416f5da2f3df3b0
Kaleev Vasiliy <62549406+Kaleev1972@users.noreply.github.com>
2020-11-17 10:29:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+"use strict"
+
+//1 пункт
+  /* let num=3;
+  alert (num) */; 
+
+//2 пункт 
+    /* let a=10;
+    let b=2;
+    alert(a+b);
+   // console.log (a+b);
+    alert(a-b); 
+    //console.log (a-b);
+
+    alert(a*b); 
+    //console.log (a*b);
+
+    alert(a/b); 
+   // console.log (a:b); */
+
+//3 пункт
+   /*  let c=15;
+    let d=2; 
+    let result = c+d;
+    // console.log (result); */
+
+//4 пункт 
+    /* let a=10;
+    let b=2;
+    let c=5;
+    console.log (a+b+c); */
+	/* function sum(a,b,c) {
+		return a+b+c;
+	}
+    console.log(sum(10,2,5));  */
+//5 пункт
+   /*  let a = 17;
+    let b = 10;
+    let c = a - b;
+    let d = 7;
+    let Result = c + d;
+    console.log ("Результат сложения переменных с и d: " + Result); */  
+	
+
+//6 пункт
+     /* let str = "Привет, Мир!";
+     console.log (str);  */
+
+ 
+//7 пункт
+    /* let str1 ='Привет,'
+    let str2 ='Мир!'
+    let result=str1+ ' ' + str2;
+    console.log (result); */ 
+	
+// 8 пункт 
+    /* let name ='%Алина%';
+	let result = 'Привет, ' + name +'!';
+	console.log(result); */
+
+//9 пункт
+   /*  let str ='Мне';
+    let age ='19 лет';
+    let result=str+ ' ' + age+'!';
+    console.log (result);  */
+
+    /* function sayYes(age) {
+        let me ='Мне';
+        let str = me+ ' ' + age + ' лет!';
+		return str;
+        }
+        console.log(sayYes(19)); */
+       
